5bacd983416971c11942a4d8b768be383d13e2f8
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-02-26 13:15:38 -0600

    Update hls4ML_BDT.py

--- BDT/hls4ML_BDT.py
+++ BDT/hls4ML_BDT.py
@@ -1,1 +1,20 @@
+import plotting
+import conifer
 
+
+#Covert model to FPGA firmware with `conifer`
+cfg = conifer.backends.xilinxhls.auto_config()
+
+# print the config
+print('Default Configuration\n' + '-' * 50)
+plotting.print_dict(cfg)
+print('-' * 50)
+
+# modify the config
+cfg['OutputDir'] = '/eos/user/s/swaldych/smart_pix/labels/generated_firmware_files' #where to put all generated firmware files
+cfg['XilinxPart'] = 'xcu250-figd2104-2L-e' #the part number for an FPGA. Taken from example (Alveo U50)
+
+# print the config again
+print('Modified Configuration\n' + '-' * 50)
+plotting.print_dict(cfg)
+print('-' * 50)
