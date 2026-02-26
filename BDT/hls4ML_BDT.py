79a7403cb9d977031bd9dfe295365530626399ed
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-02-26 13:49:20 -0600

    Update hls4ML_BDT.py

--- BDT/hls4ML_BDT.py
+++ BDT/hls4ML_BDT.py
@@ -1,5 +1,51 @@
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve, classification_report
+import pandas as pd
+import numpy as np
 import conifer
 import plotting
+
+models_dir = '/eos/user/s/swaldych/smart_pix/labels/models'
+results_dir = '/eos/user/s/swaldych/smart_pix/labels/results'
+
+sensor_geom = "50x12P5x150_0fb"
+threshold = 0.2 #in GeV
+tag = f"{sensor_geom}_0P{str(threshold - int(threshold))[2:]}thresh"
+
+dfx = pd.read_csv(f"/eos/user/s/swaldych/smart_pix/labels/preprocess/FullPrecisionInputTrainSet_{tag}.csv") #y-local
+dfy = pd.read_csv(f"/eos/user/s/swaldych/smart_pix/labels/preprocess/TrainSetLabel_{tag}.csv") 
+pt=pd.read_csv(f"/eos/user/s/swaldych/smart_pix/labels/preprocess/TrainSetPt_{tag}.csv")
+
+X = dfx.values
+y = dfy.values
+real_pt=pt.values
+
+X_train, X_test, y_train, y_test, pt_train, pt_test = train_test_split(
+    X, y, real_pt, test_size=0.2, shuffle=True, random_state=42) #we are saying split arrays the same way
+
+#  Convert data to XGBoost DMatrix format
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dtest = xgb.DMatrix(X_test, label=y_test)
+
+params = {
+    "objective": "multi:softprob",  # "softmax option produces a trained Booster object whose predict method returns a 1d array of predicted labels, whereas the softprob option produces a trained Booster object whose predict method returns a 2d array of predicted probabilities"
+    "eval_metric": "auc",  # AUC for evaluation
+    "max_depth": 5,  # Tree depth
+    "eta": 0.05,  # Learning rate
+    "seed": 42,  # Reproducibility
+    "num_class":3 #three classes...0,1,2 
+}
+
+model = xgb.train(
+    params,
+    dtrain,
+    num_boost_round=200,
+    evals=[(dtrain, "train"), (dtest, "test")],
+    early_stopping_rounds=10,
+    verbose_eval=10  # Print progress every 10 iterations
+)
 
 
 #Covert model to FPGA firmware with `conifer`
@@ -20,6 +66,6 @@ plotting.print_dict(cfg)
 print('-' * 50)
 
 # convert the model to the conifer representation
-conifer_model = conifer.converters.convert_from_sklearn(clf, cfg)
+conifer_model = conifer.converters.convert_from_sklearn(model, cfg)
 # write the project (writing HLS project to disk)
 conifer_model.write()
