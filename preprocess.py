a49497e3a59b24d4070592ad8cf29a5db9806d71
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-03-12 16:13:09 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -13,7 +13,7 @@ noise_threshold = 400
 sensor_geom = '50x12P5x150_0fb'
 dataset_savedir =  'eos/user/s/swaldych/smart_pix/labels/preprocess/' # for save loc of final datasets
 
-dirtrain = 'eos/user/s/swaldych/smart_pix/labels/'
+dirtrain = '/eos/user/s/swaldych/smart_pix/labels'
 # /location/of/parquets/smartpixels/dataset_2s/dataset_2s_50x12P5_parquets/unflipped
 dftrain = pd.read_parquet(dirtrain+'labels_d16401.parquet')
 print(dftrain.head())
