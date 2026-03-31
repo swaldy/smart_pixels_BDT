e34fbcd09d91404ec7a6d364efad584ff6d8eb7b
Sarah Waldych <101739736+swaldy@users.noreply.github.com>
2026-03-31 13:57:51 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -15,7 +15,7 @@ if not os.path.exists(directory):
 sensor_geom = '50x12P5'
 noise_threshold=0
 threshold = 0.2
-output_file = open(directory+"output_"+sensor_geom+"_"+str(noise_thresholdt)+"NoiseThresh.txt", "w")
+output_file = open(directory+"output_"+sensor_geom+"_"+str(noise_threshold)+"NoiseThresh.txt", "w")
 
 qm_charge_levels = [400, 1600, 2400]
 qm_quant_values = [0,1,2,3]
@@ -23,7 +23,7 @@ qm_quant_values = [0,1,2,3]
 train_dataset_name = 'dataset_3s' # for train datasets
 test_dataset_name = 'dataset_2s' # for location of test (physical pT) datasets
 
-dataset_savedir = f'/eos/user/s/swaldych/smart_pix/dataset_3s_{noise_threshold}NoiseThresh'
+dataset_savedir = f'/eos/user/s/swaldych/smart_pix/dataset_3s_{noise_threshold}NoiseThresh_testing_quantized'
 
 output_file.write("=========="+"\n")
 output_file.write("Train dataset prod. beginning: "+str(threshold)+" thresh"+"\n")
